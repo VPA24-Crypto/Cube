e3f5bf2b2fb96d7283c527c13d0e31a1ac647ab8
Vadim <prokudinwadim@mail.ru>
2020-01-18 18:23:07 +0500

    Cube

---
+++ Assets/Script/ButtonColor.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+public class ButtonColor : MonoBehaviour
+{
+    public GameObject Gd;
+    public Button bt;
+    public Color cl;
+    
+    public string data;
+
+    void Start()
+    {
+        data = File.ReadAllText("Save/" + gameObject.GetInstanceID() + ".txt");
+        JsonUtility.FromJsonOverwrite(data, this);
+        SetCol();
+    }
+
+    void Update()
+    {
+        if (Gd.GetComponent<Renderer>().material.color == Color.white)
+        {
+            bt.GetComponentInChildren<Text>().text = "White";
+        }
+        else
+        {
+            bt.GetComponentInChildren<Text>().text = "Blue";
+        }
+    }
+
+    public void ColorAndText()
+    {
+        if (Gd.GetComponent<Renderer>().material.color != Color.blue)
+        {
+            Gd.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        else
+        {
+            Gd.GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+
+
+    public void Coll()
+    {
+        cl = Gd.GetComponent<Renderer>().material.color;
+    }
+
+    public void SetCol()
+    {
+        Gd.GetComponent<Renderer>().material.color = cl;
+    }
+
+    void OnApplicationQuit()
+    {
+        Coll();
+        data = JsonUtility.ToJson(this, true);
+        File.WriteAllText("Save/" + gameObject.GetInstanceID() + ".txt", data);
+
+    }
+
+}
+
---
+++ Assets/Script/ClickCube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ClickCube : MonoBehaviour
+{
+    private Color bafColor = Color.blue;
+    private Color hovColor = Color.white;
+    private Renderer render;
+
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+        //render.material.color = hovColor;
+    }
+    void OnMouseDown()
+    {
+       if(render.material.color != bafColor)
+        {
+            render.material.color = bafColor;
+        }
+       else
+        {
+            render.material.color = hovColor;
+        }
+    }
+
+
+
+}
